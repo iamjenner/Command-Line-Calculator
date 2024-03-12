801f39b7f0e678a30f95aff8ffef567df23fbfbd
Ijenna Okonkwo <85650455+iamjenner@users.noreply.github.com>
2024-03-11 21:04:02 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        MyStack<Integer> stack = new MyArrayList<>();
+        stack.push(1);
+        stack.push(6);
+        stack.push(10);
+
+        System.out.println(stack);
+
+
+    }
+}
